7927c89d23fb0be653513f2acb372e1a540f2f3c
matthewcurrier <currier.matthew@gmail.com>
2020-09-07 16:52:07 -0700

    super small cahnge

--- app.R
+++ app.R
@@ -12,13 +12,16 @@ library(shiny)
 library(shiny)
 
 ui <- fluidPage(
-  textInput("name", "What's your name?"),
-  textOutput("greeting")
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", label = "If y is", min = 1, max = 50, value = 30),
+  "then x times y is:",
+  textOutput("product")
 )
 
 server <- function(input, output, session) {
-  output$greeting  <- renderText({
-    paste0("Hello ", input$name)
+  
+  output$product <- renderText({ 
+    input$x * input$y
   })
 }
 
